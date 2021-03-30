01d82d1bc3b1a816ba637e579429655bc730edc4
rbiedrawa <biedrawa.rafal@gmail.com>
2021-03-30 10:21:48 +0200

    Init commit

---
+++ src/test/java/com/rbiedrawa/hexagonal/app/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.rbiedrawa.hexagonal.app;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
