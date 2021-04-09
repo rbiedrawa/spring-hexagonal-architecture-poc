533783889b0661b55e31adb671888cdf285e5cc2
rbiedrawa <biedrawa.rafal@gmail.com>
2021-04-09 10:59:12 +0200

    Added sample way to eliminate spring framework from 'business' package

--- src/main/resources/db/migration/V1__orders_ddl.sql
+++ src/main/resources/db/migration/V1__orders_ddl.sql
@@ -1,4 +1,4 @@
-create table orders
+create table ORDERS
 (
     id                 uuid not null,
     customer_full_name varchar(255),
--- src/test/resources/sql/createOrder-cb1c6b78-ed68-4d68-9699-a1b6014bad55.sql
+++ src/test/resources/sql/createOrder-cb1c6b78-ed68-4d68-9699-a1b6014bad55.sql
@@ -1,2 +1,2 @@
-INSERT INTO orders
+INSERT INTO ORDERS (id, customer_full_name, product_name, total_price, status)
 VALUES ('cb1c6b78-ed68-4d68-9699-a1b6014bad55', 'Sql Test', '@SQL @SQL', 999.99, 'REJECTED');
